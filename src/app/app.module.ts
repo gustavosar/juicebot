2b7b1768438afb921c90a46fcf332763558d789c
Gustavo Ramos <gustavo.ramos@corp.proreserve>
2019-05-10 18:22:13 -0300

    slider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { SwiperModule } from 'angular2-useful-swiper';
+
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
+import { SliderComponent } from './slider/slider.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    FooterComponent
+    FooterComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SwiperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/slider/slider.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { compileFactoryFunction } from '@angular/compiler/src/render3/r3_factory';
+
+@Component({
+  selector: 'app-slider',
+  templateUrl: './slider.component.html',
+  styleUrls: ['./slider.component.styl']
+})
+export class SliderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() { }
+
+  title = 'NG7Swiper';
+ 
+  config: SwiperOptions = {
+    autoplay: 5000, // Autoplay option having value in milliseconds
+    initialSlide: 0, // Slide Index Starting from 0
+    slidesPerView: 1, // Slides Visible in Single View Default is 1
+    pagination: '.swiper-pagination', // Pagination Class defined
+    paginationType: 'fraction',
+    paginationClickable: true, // Making pagination dots clicable
+    nextButton: '.swiper-button-next', // Class for next button
+    prevButton: '.swiper-button-prev', // Class for prev button
+    loop: true,
+    effect: 'fade',
+
+  };
+}
