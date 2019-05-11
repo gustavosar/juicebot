6744f418d5539933c5d36b8a94d543bc99c41f7d
Gustavo Sa <gustavosa@MacBook-Pro-de-Gustavo.local>
2019-05-10 21:52:33 -0300

    naigation style

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -15,11 +15,10 @@ export class SliderComponent implements OnInit {
   title = 'NG7Swiper';
  
   config: SwiperOptions = {
-    autoplay: 5000, // Autoplay option having value in milliseconds
+    autoplay: 95000, // Autoplay option having value in milliseconds
     initialSlide: 0, // Slide Index Starting from 0
     slidesPerView: 1, // Slides Visible in Single View Default is 1
     pagination: '.swiper-pagination', // Pagination Class defined
-    paginationType: 'fraction',
     paginationClickable: true, // Making pagination dots clicable
     nextButton: '.swiper-button-next', // Class for next button
     prevButton: '.swiper-button-prev', // Class for prev button
